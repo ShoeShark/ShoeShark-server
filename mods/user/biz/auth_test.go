dd3619aa6aaee715b72ccf6bc697e6e6b7e6e6f2
yealou <yanlsix@163.com>
2024-05-22 00:04:41 +0800

    验证 优化/修复

--- mods/user/biz/auth_test.go
+++ mods/user/biz/auth_test.go
@@ -2,8 +2,8 @@ package biz
 
 import (
 	"crypto/ecdsa"
-	"encoding/hex"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/shoe-shark/shoe-shark-service/pkg/ethers"
 	"testing"
@@ -27,20 +27,29 @@ func TestGenerateAndVerifySignature(t *testing.T) {
 	nonce := "dc408761e0b19a813bd3bbf9f3e15ba0"
 
 	signature, err := GenerateSignature(privateKey, nonce)
-	fmt.Println("signature:", hex.EncodeToString(signature))
+	fmt.Println("signature:", signature)
 	if err != nil {
 		t.Fatalf("Failed to generate signature: %v", err)
 	}
 
 	// 验证签名
-	isVaild := ethers.VerifySignature(accountAddress, hex.EncodeToString(signature), nonce)
+	isVaild := ethers.VerifySignature(accountAddress, signature, nonce)
 	if !isVaild {
 		t.Fatalf("Signature verification failed")
 	}
 }
 
-func GenerateSignature(privateKey *ecdsa.PrivateKey, nonce string) ([]byte, error) {
-	// 计算消息哈希
-	messageHash := crypto.Keccak256Hash([]byte(nonce))
-	return crypto.Sign(messageHash.Bytes(), privateKey)
+func GenerateSignature(privateKey *ecdsa.PrivateKey, nonce string) (string, error) {
+	message := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(nonce), nonce)
+	messageHash := crypto.Keccak256Hash([]byte(message))
+
+	signature, err := crypto.Sign(messageHash.Bytes(), privateKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign message: %v", err)
+	}
+
+	// Adjust the V value (0 or 1) to 27 or 28
+	signature[64] += 27
+
+	return hexutil.Encode(signature), nil
 }
